bfb3c85f68d8f7b5048d8d11138486536c5efac1
TLatu2020 <60384040+TLatu2020@users.noreply.github.com>
2021-01-04 19:05:16 +1100

    code improved

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 const links = [{
     label: "Week1 notes",
-    url: "week1"
+    url: "week1/index.html"
 }]
+
+links.forEach(link => {
+    let list = document.createElement("li");
+    let week = document.createElement("a");
+
+    week.textContent = link.label;
+    week.setAttribute("href", link.url);
+
+    list.append(week);
+
+    document.querySelector('ol').appendChild(list);
+
+})
