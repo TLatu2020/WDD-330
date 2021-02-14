1f321745379449201d332a9843f626259e5eba07
TLatu2020 <60384040+TLatu2020@users.noreply.github.com>
2021-02-14 16:52:30 +1100

    code improved

--- challenges/js/main.js
+++ challenges/js/main.js
@@ -1,19 +1,15 @@
 import Todos from './ToDos.js';
 
-// window.onload = function() {
-//     const ul = document.getElementById('list');
-//     let todo = new Todos('myTodo', ul);
-//     todo.init();
-// }
+window.onload = function() {
+    const ul = document.getElementById('list');
+    let todo = new Todos('myTodo', ul);
+    todo.init();
+}
 
-const ul = document.getElementById('list');
-let todo = new Todos('myTodo', ul);
-todo.init();
-// import ToDos from './ToDos.js';
+// const ul = document.getElementById('list');
+// let todo = new Todos('myTodo', ul);
+// todo.init();
 
-// const toDo = new ToDos("#list", "myList");
-
-// toDo.listTodos();
 
 WebFont.load({
     google: {
