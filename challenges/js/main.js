bf1bca758913b2efd6f8326f9ef57e8e6e750ef6
TLatu2020 <60384040+TLatu2020@users.noreply.github.com>
2021-02-14 16:33:32 +1100

    windows.onload removed

--- challenges/js/main.js
+++ challenges/js/main.js
@@ -1,11 +1,14 @@
 import Todos from './Todos.js';
 
-window.onload = function() {
-    const ul = document.getElementById('list');
-    let todo = new Todos('myTodo', ul);
-    todo.init();
-}
+// window.onload = function() {
+//     const ul = document.getElementById('list');
+//     let todo = new Todos('myTodo', ul);
+//     todo.init();
+// }
 
+const ul = document.getElementById('list');
+let todo = new Todos('myTodo', ul);
+todo.init();
 // import ToDos from './ToDos.js';
 
 // const toDo = new ToDos("#list", "myList");
