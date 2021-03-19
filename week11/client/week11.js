0a8e9fb76b5e19e3473447a64d549e88d0d94204
TLatu2020 <60384040+TLatu2020@users.noreply.github.com>
2021-03-19 19:19:56 +1100

    team activity fixed

--- week11/client/week11.js
+++ week11/client/week11.js
@@ -1,43 +1,39 @@
 import Auth from './auth.js';
-
-import { Errors, makeRequest } from './authHelpers';
-
+import { Errors, makeRequest } from './authHelpers.js';
 // makeRequest('login', 'POST', {
-//     password: 'user1',
-//     email: 'user1@email.com'
-// })
+//   password: 'user1',
+//   email: 'user1@email.com'
+// });
 
 const myErrors = new Errors('errors');
-const myAuth = new Auth(myErrors)
+const myAuth = new Auth(myErrors);
 
 const loginForm = document.getElementById('login');
 loginForm.querySelector('button').addEventListener('click', () => {
     myAuth.login(getPosts);
 });
-
 async function getPosts() {
     try {
-        const data = await makeRequest('post', 'GET', null, myAuth.token);
-
+        const data = await makeRequest('posts', 'GET', null, myAuth.token);
+        // make sure the element is shown
         document.getElementById('content').classList.remove('hidden');
         console.log(data);
-        let ul = document.getElementById('list');
+        var ul = document.getElementById('list');
         ul.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
-            let li = document.createElement('li');
+        for (var i = 0; i < data.length; i++) {
+            var li = document.createElement('li');
             li.appendChild(document.createTextNode(data[i].title));
             ul.appendChild(li);
         }
         myErrors.clearError();
     } catch (error) {
+        // if there were any errors display them
         myErrors.handleError(error);
     }
 }
-
 document.getElementById('createSubmit').addEventListener('click', () => {
     createPost();
 });
-
 async function createPost() {
     const form = document.forms.postForm;
     console.dir(form);
@@ -49,6 +45,7 @@ async function createPost() {
         };
         try {
             const res = await makeRequest('posts', 'POST', data, myAuth.token);
+            console.log('Post create:', data);
             form.title.value = '';
             form.content.value = '';
             getPosts();
