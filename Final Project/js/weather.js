5de35e176314145712ff764344898c281d43a982
TLatu2020 <60384040+TLatu2020@users.noreply.github.com>
2021-03-29 19:15:49 +1100

    current weather info added

--- Final Project/js/weather.js
+++ Final Project/js/weather.js
@@ -4,6 +4,27 @@ export const getLocation = navigator.geolocation.getCurrentPosition(position => 
 
     const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=7cbff6ff955122bea5c143b87e2663a5&units=metric`;
 
+    const currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7cbff6ff955122bea5c143b87e2663a5&units=metric`;
+
+    fetch(currentURL)
+        .then((response) => response.json())
+        .then((jsObject) => {
+
+            let temp = jsObject.main.temp;
+            let windSpeed = jsObject.wind.speed;
+
+            document.getElementById('currentCondition').textContent = jsObject.weather[0].main;
+
+            //Current Temp
+            document.getElementById('currentTemp').textContent = Math.round(jsObject.main.temp) + " °F";
+
+            //Highest Temp
+            document.getElementById('high-temp').textContent = Math.round(jsObject.main.temp_max) + " °F";
+
+            //Humidity Level
+            document.getElementById('humidity').textContent = jsObject.main.humidity + " %";
+        })
+
     fetch(forecastURL)
         .then((response) => response.json())
         .then((jsObject) => {
@@ -20,7 +41,6 @@ export const getLocation = navigator.geolocation.getCurrentPosition(position => 
 
             for (let i = 0; i < 1; i++) {
                 fiveDays.forEach(forecast => {
-                    // console.log(forecast);
 
                     let card = document.createElement('section');
                     let weekDay = document.createElement('p');
